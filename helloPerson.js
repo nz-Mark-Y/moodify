81d436ab96bf07fe5e09c947c0bb5d9e6f4e8dd3
Mark Yep <mark_yep@hotmail.com>
2016-08-06 13:37:01 +1200

    Add project files and folders

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Hello World 002";
+document.body.innerHTML = greeter(user);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "Hello World 002";
+
+document.body.innerHTML = greeter(user);  
